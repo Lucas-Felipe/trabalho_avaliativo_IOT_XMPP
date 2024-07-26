68ec95567f5bac47d41efc3a5baa4d78770a1306
lucas.felipe <lucas.felipe@mprn.mp.nr>
2024-07-25 21:31:05 -0300

    tentando com slixmpp

--- xmpp_env/xmpp.py
+++ xmpp_env/xmpp.py
@@ -1,35 +1,16 @@
-import slixmpp
-import logging
+import xmpp
 
-# Configura o logging para depuração
-logging.basicConfig(level=logging.DEBUG)
+jabberid = "admin@localhost"
+password = "admin"
+receiver = "amigoteste@localhost"
+message  = "hello world"
 
-class SendMsgBot(slixmpp.ClientXMPP):
-    def __init__(self, jid, password, recipient, message):
-        super().__init__(jid, password)
-        self.recipient = recipient
-        self.message = message
+def main():
+    jid = xmpp.protocol.JID(jabberid)
+    connection = xmpp.Client(server=jid.getDomain(), debug=True)
+    connection.connect()
+    connection.auth(user=jid.getNode(), password=password, resource=jid.getResource())
+    connection.send(xmpp.protocol.Message(to=receiver, body=message))
 
-        self.add_event_handler("session_start", self.start)
-        self.add_event_handler("failed_auth", self.failed_auth)
-
-    async def start(self, event):
-        print("Session started.")
-        self.send_presence()
-        await self.get_roster()
-        self.send_message(mto=self.recipient, mbody=self.message, mtype='chat')
-        self.disconnect()
-
-    def failed_auth(self, event):
-        print("Authentication failed")
-        self.disconnect()
-
-if __name__ == '__main__':
-    jid = "admin@localhost"
-    password = "admin"
-    recipient = "amigoteste@localhost"
-    message = "hello world"
-
-    xmpp = SendMsgBot(jid, password, recipient, message)
-    xmpp.connect()
-    xmpp.process(forever=False)
+if __name__ == "__main__":
+    main()
